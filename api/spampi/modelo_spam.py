942ec1481244cbddd778d428215c0fb13f6fd239
Ezequiel Raigorodsky Ramos <ezequielraigorodsky@gmail.com>
2020-11-28 19:30:22 -0300

    probando cargar modelo

--- api/spampi/modelo_spam.py
+++ api/spampi/modelo_spam.py
@@ -17,30 +17,30 @@ stopwords_english = stopwords.words('english')
 stopwords_english.pop( stopwords_english.index('re') ) # Saco 're' de stopwords
 
 def get_wordnet_pos(word):
- """Map POS tag to first character lemmatize() accepts"""
- tag = nltk.pos_tag([word])[0][1][0].upper()
- tag_dict = {"J": wordnet.ADJ,
-             "N": wordnet.NOUN,
-             "V": wordnet.VERB,
-             "R": wordnet.ADV}
- return tag_dict.get(tag, wordnet.NOUN) # Por default devuelve sustantivo
+    """Map POS tag to first character lemmatize() accepts"""
+    tag = nltk.pos_tag([word])[0][1][0].upper()
+    tag_dict = {"J": wordnet.ADJ,
+                "N": wordnet.NOUN,
+                "V": wordnet.VERB,
+                "R": wordnet.ADV}
+    return tag_dict.get(tag, wordnet.NOUN) # Por default devuelve sustantivo
 lemmatizer = WordNetLemmatizer()
 
 def lemmatize(word):
- lemmatized = lemmatizer.lemmatize(word, get_wordnet_pos(word)) # Lemmatizacion flexible (según POS)
- return lemmatized
+    lemmatized = lemmatizer.lemmatize(word, get_wordnet_pos(word)) # Lemmatizacion flexible (según POS)
+    return lemmatized
 
 def remove_basura(mail):
-  sin_basura = re.sub('(Subject|[\W_\d])', ' ', mail) # Saco números, puntuación, etc. y la palabra subject
-  sin_basura = re.sub(' [a-zA-Z] ', ' ', sin_basura) # Saco letras sueltas (a, s, t...)
-  sin_basura = re.sub(' {2,}', ' ', sin_basura) # Saco espaciado de sobra
-  sin_basura = re.sub('^\s', '', sin_basura) # Saco el primer espacio de cada oracion
-  return sin_basura
+    sin_basura = re.sub('(Subject|[\W_\d])', ' ', mail) # Saco números, puntuación, etc. y la palabra subject
+    sin_basura = re.sub(' [a-zA-Z] ', ' ', sin_basura) # Saco letras sueltas (a, s, t...)
+    sin_basura = re.sub(' {2,}', ' ', sin_basura) # Saco espaciado de sobra
+    sin_basura = re.sub('^\s', '', sin_basura) # Saco el primer espacio de cada oracion
+    return sin_basura
 
 def prune_words(mail):
-  tokens = nltk.word_tokenize( remove_basura(mail), language='english' ) # Tokenizo (equivale al join de vicky)
-  lemmatized_not_stopword = [lemmatize(token.lower()) for token in tokens if token.lower() not in stopwords_english] # OJO que stopwords incluye 're', aca la saque
-  return lemmatized_not_stopword
+    tokens = nltk.word_tokenize( remove_basura(mail), language='english' ) # Tokenizo (equivale al join de vicky)
+    lemmatized_not_stopword = [lemmatize(token.lower()) for token in tokens if token.lower() not in stopwords_english] # OJO que stopwords incluye 're', aca la saque
+    return lemmatized_not_stopword
 
 
 class ModeloSpam(object):
--- api/spampi/views.py
+++ api/spampi/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+import os
+import sys
+cwd = os.path.join(
+        os.getcwd(),
+        "spampi"
+    )
+sys.path.append(cwd)
+import modelo_spam
+# import spampi.modelo_spam as modelo_spam
 
-# Create your views here.
+def process_email(request):
+    cwd = os.path.join(
+        os.getcwd(),
+        "spampi"
+    )
+    mod_trained = modelo_spam.ModeloSpam.load_spam_model(in_path=cwd)
+    texto_test = "buy viagara king of nigeria buy"
+    result = mod_trained.predict_email(texto_test)
+    print(result)
