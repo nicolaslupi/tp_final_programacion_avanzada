ab28320a51eb9e10631af28387fa001677590bf4
Ezequiel Raigorodsky Ramos <ezequielraigorodsky@gmail.com>
2020-11-28 21:44:26 -0300

    El modelo entrenado se carga exitosamente y deuelve en la respuesta de http el resultado. Proximos pasos en este contexto es darle el formato de API pero es construir sobre esto.

--- api/spampi/modelo_spam.py
+++ api/spampi/modelo_spam.py
@@ -1,47 +1,12 @@
 import pickle
-
-import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize 
-import string
-string.punctuation
-from nltk.corpus import stopwords
-nltk.download('stopwords')
-nltk.download('wordnet')
-nltk.download('averaged_perceptron_tagger')
-from nltk.stem import WordNetLemmatizer
-from nltk.corpus import wordnet
-nltk.download('punkt')
-
-stopwords_english = stopwords.words('english')
-stopwords_english.pop( stopwords_english.index('re') ) # Saco 're' de stopwords
-
-def get_wordnet_pos(word):
-    """Map POS tag to first character lemmatize() accepts"""
-    tag = nltk.pos_tag([word])[0][1][0].upper()
-    tag_dict = {"J": wordnet.ADJ,
-                "N": wordnet.NOUN,
-                "V": wordnet.VERB,
-                "R": wordnet.ADV}
-    return tag_dict.get(tag, wordnet.NOUN) # Por default devuelve sustantivo
-lemmatizer = WordNetLemmatizer()
-
-def lemmatize(word):
-    lemmatized = lemmatizer.lemmatize(word, get_wordnet_pos(word)) # Lemmatizacion flexible (según POS)
-    return lemmatized
-
-def remove_basura(mail):
-    sin_basura = re.sub('(Subject|[\W_\d])', ' ', mail) # Saco números, puntuación, etc. y la palabra subject
-    sin_basura = re.sub(' [a-zA-Z] ', ' ', sin_basura) # Saco letras sueltas (a, s, t...)
-    sin_basura = re.sub(' {2,}', ' ', sin_basura) # Saco espaciado de sobra
-    sin_basura = re.sub('^\s', '', sin_basura) # Saco el primer espacio de cada oracion
-    return sin_basura
-
-def prune_words(mail):
-    tokens = nltk.word_tokenize( remove_basura(mail), language='english' ) # Tokenizo (equivale al join de vicky)
-    lemmatized_not_stopword = [lemmatize(token.lower()) for token in tokens if token.lower() not in stopwords_english] # OJO que stopwords incluye 're', aca la saque
-    return lemmatized_not_stopword
-
+import os
+import sys
+cwd = os.path.join(
+        os.getcwd(),
+        "spampi"
+    )
+sys.path.append(cwd)
+import model_preprop_funcs
 
 class ModeloSpam(object):
     def __init__(self, transform_entrenado, pca_entrenado, svm_entrenado):
--- api/spampi/views.py
+++ api/spampi/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 import os
 import sys
 cwd = os.path.join(
@@ -6,15 +6,13 @@ cwd = os.path.join(
         "spampi"
     )
 sys.path.append(cwd)
-import modelo_spam
-# import spampi.modelo_spam as modelo_spam
+import modelo_spam  # noqa
+mod_trained = modelo_spam.ModeloSpam.load_spam_model(in_path=cwd)
+
 
 def process_email(request):
-    cwd = os.path.join(
-        os.getcwd(),
-        "spampi"
-    )
-    mod_trained = modelo_spam.ModeloSpam.load_spam_model(in_path=cwd)
     texto_test = "buy viagara king of nigeria buy"
     result = mod_trained.predict_email(texto_test)
-    print(result)
+    mail_type = "Spam" if result == 1 else "Ham"
+    mesg = f"The email processed is: {mail_type}!"
+    return HttpResponse(mesg)
