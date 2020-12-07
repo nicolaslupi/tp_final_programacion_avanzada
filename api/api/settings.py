a84803cbfacb0ab47341396eaef2c87a4b1cd1ba
lipinoelbreve <nicolupi.2@gmail.com>
2020-12-07 13:18:29 -0300

    Implemento autenticacion via Token, agrego libreria de JWT a requirements.txt

--- api/api/settings.py
+++ api/api/settings.py
@@ -30,6 +30,16 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+      'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+      'rest_framework.authentication.SessionAuthentication',
+    ),
+}
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
--- api/api/urls.py
+++ api/api/urls.py
@@ -3,9 +3,11 @@
 from django.contrib import admin
 from django.urls import path
 from spampi import views
+from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-token-auth/', obtain_jwt_token),
     path('quota_info/', views.quota_info.as_view()),
     path("process_email/", views.process_email.as_view()),
     path("history/<int:N_EMAILS>/", views.history.as_view()),
